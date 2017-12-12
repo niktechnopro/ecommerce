b6050f34ff3dc12f68ebf3ecfce872af31a9194c
Nikolas Bogucarskis <niktechnopro@gmail.com>
2017-12-12 10:34:54 -0500

    added, store, provider, config for back-end, and few other things

--- ecommerce/front-end/src/index.js
+++ ecommerce/front-end/src/index.js
@@ -12,9 +12,30 @@ import {createStore, applyMiddleware} from 'redux'; //these are in brockets - th
 
 // createStore needs a reducer/ specifically a Root reducer
 import RootReducer from './reducers/RootReducer';
+// we are going to need ajax a lot. we will use it in our Redux Actions which means... we need redux-promise
+import reduxPromise from 'redux-promise';
+// create the store ... the ugly way
+import { Provider } from 'react-redux';
 
+//all imports must be before first const
+
+const theStore = applyMiddleware(reduxPromise)(createStore)(RootReducer);
+//the friendly way/...
+// const middleWare = applyMiddleware(reduxPromise);
+// const storeWithMid = middleWare(createStore);
+// const theStore = storeWithMid(RootReducer);
+
+// if I see x()() - x is returning a function and that function also returning a function
+// We have set up Redux. Now we need a way to tell React about it - anser: PROVIDER!
+
+// Hand render the Provide and hand Provider theStore
+// Put App inside of the provider, so that everything inside of App,
+
+// will know about the Provider/theStore
 ReactDOM.render(
-	<App />, 
+	<Provider store={theStore} >
+	<App />
+	</Provider>, 
 	document.getElementById('root')
 	);
 // registerServiceWorker();
